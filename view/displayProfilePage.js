fd105240adc8f17e46dc014b0ba96957d9378c3d
Ellert Smári Kristbergsson <ellertsmari@gmail.com>
2018-04-17 09:50:01 +0000

    made a profile page for view

--- view/displayProfilePage.js
+++ view/displayProfilePage.js
@@ -1,1 +1,9 @@
 //síða sem ég ætla að nota til þess að sýna dæmi
+
+const displayProfilePage = (userName, pictureURl, phoneNumber) => {
+    document.getElementById("container").innerHTML= `
+    username: ${userName} <br>
+    phoneNumber: ${phoneNumber} <br>
+    pictureURL: <img src="${pictureURl}"> 
+    `
+}
