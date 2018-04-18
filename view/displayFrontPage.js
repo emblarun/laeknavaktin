e848f45fc29a9d30da756847fcf8f839eeaf55f0
birgittarun92 <birgittarun92@gmail.com>
2018-04-18 09:40:45 +0000

    baetti vid divi aftur

--- view/displayFrontPage.js
+++ view/displayFrontPage.js
@@ -3,7 +3,7 @@ const container = document.getElementById("container")
 const getFirstPage = () => {
     container.innerHTML+= `
     <div>
-        <img src="img/logo.png"> 
+        <img src="img/logo.png">
         <h2> Velkomin/n á </h2>
         <h1> Læknavaktina </h12>
         <h3> Hringdu á 112 ef um alvarleg veikindi eru að ræða </h3>
