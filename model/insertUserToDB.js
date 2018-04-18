6a1ea320f45b1443a0240a719a5664f4609b5d60
MyraMidnight <myramidnight87@gmail.com>
2018-04-18 14:34:59 +0000

    redid it again...

--- model/insertUserToDB.js
+++ model/insertUserToDB.js
@@ -1,1 +1,12 @@
 // insert userName and phoneNumber
+const insertUserToDB = (user)=>{
+		
+	db.collection("users").doc(user.phoneNumber).set({ //Creates a user document with UID
+		'phone': user.phoneNumber,
+	}).then(()=>{
+	  console.log("// currentUser updated!");
+	  currentUser = user;
+	}).catch(()=>{
+	  console.log("error writing user profile: ", error);
+	});	
+}
