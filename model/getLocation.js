d0caed73fbbadd5d58753165f8137d858430b53a
birgittarun92 <birgittarun92@gmail.com>
2018-04-18 10:30:35 +0000

    settum query snapshot

--- model/getLocation.js
+++ model/getLocation.js
@@ -1,1 +1,20 @@
 //Get geolocation coordinates
+
+const getHeilsugaeslur = (heilsugaeslaId) => {
+    var docRef = db.collection("heilsugaeslur");
+    return new Promise((resolve) => {
+        docRef.get().then((querySnapshot) => {
+            querySnapshot.forEach(function(doc){
+                if (doc.exists) {
+                    resolve(doc.data());
+                } else {
+                    resolve(null);
+                }
+            })
+            
+        }).catch((err) => {
+            console.error(err);
+            resolve(null);
+        });
+    });
+}
