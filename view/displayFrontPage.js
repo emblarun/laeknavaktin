2ca4397b00aaa71f39503ccc91f259a1668380d6
birgittarun92 <birgittarun92@gmail.com>
2018-04-18 09:37:42 +0000

    baetti vid div fyrir authenticator

--- view/displayFrontPage.js
+++ view/displayFrontPage.js
@@ -3,31 +3,12 @@ const container = document.getElementById("container")
 const getFirstPage = () => {
     container.innerHTML+= `
     <div>
-      <img src="img/logo.png">
-      <h2> Velkomin/n á </h2>
-      <h1> Læknavaktina </h12>
-      <h3> Hringdu á 112 ef um alvarleg veikindi eru að ræða </h3>
+        <img src="img/logo.png"> 
+        <h2> Velkomin/n á </h2>
+        <h1> Læknavaktina </h12>
+        <h3> Hringdu á 112 ef um alvarleg veikindi eru að ræða </h3>
     </div>
-    <div>
-      <p> Sláðu inn símanúmer til að skrá þig inn </p>
-      <input type="tel" id="phoneInput">
-      <button id="phonebutton" onclick="signinClick()">skrá inn</button>
-    </div>
-    <div id="firebaseui-auth-container"></div>
-`
-}
-
-const getAudkenniPage = () => {
-    container.innerHTML+= `
-
-    
-    <div>
-      <p> Sláðu inn sms kóðann </p>
-      <input type="number" id="input">
-      <button id="sendAgain">Senda aftur</button>
-      <button id="afram">Áfram</button>
+    <div id="firebaseui-auth-container"> 
     </div>
 `
 }
-
-getFirstPage();
