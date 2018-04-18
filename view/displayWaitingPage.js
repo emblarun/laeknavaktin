1c204047a3413b447c81cca38bbff9b86978d681
eevaksu <eevaksu@gmail.com>
2018-04-18 12:04:34 +0000

    added Send button and function to the page

--- view/displayWaitingPage.js
+++ view/displayWaitingPage.js
@@ -1,28 +1,47 @@
 //Here comes the waiting list page (page 4) done by Eeva :)
-const displayWaitingPage = (numberInLine, queueId) => {
+getQueue().then((queue)=>{
+  waitingNumber = queue.length;
+})
+const displayWaitingPage = (waitingTime, waitingNumber) => {
     document.getElementById("container").innerHTML= `
     <div class="container">
       <div class="row">
         <div class="col-12">
           <h1>Þú átt tíma eftir:</h1>
-          <div><h1>${timeConvert((numberInLine - 1) * 15)}</h1></div>
+          <div><h1>${timeConvert(waitingTime)}</h1></div>
         </div>
+      </div>
+      <div class="row">
         <div class="col-12">
-          <p>Það eru ${numberInLine - 1} á undan þér.</p>
+          <p>Það eru ${waitingNumber} á undan þér.</p>
         </div>
+      </div>
+      <div class=row>
         <div class="col-12">
           <p>Flýttu fyrir:</p>
           <input id="reasonForVisit" type="text" placeholder="Hvað bjátar á..."></input>
         </div>
+      </div>
+      <br>
+      <div class="row">
         <div class="col-12-mt-5">
+        <!--Button to send comments to Firebase-->
+          <button id="sendComments">Senda</button>
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <div class="col-12-mt-5">
+        <!--Button to take patient out of the queue-->
           <button id="cancelVisit" type="button" class="btn btn-primary">Fara úr röðinni
           </button>
         </div>
+      </div>
       </div><!--closes row-->
     </div><!--closes container-->
-  `;
-
-  onUserCommentChange(queueId);
+    `
+    //Eventlistener for the send button!
+    document.getElementById("sendComments").addEventListener('click', insertCommentOnClick);
 }
 
 
